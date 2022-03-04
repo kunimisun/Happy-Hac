f86b998a87b511daf457930ad655cb2844c0c300
kunimisun <83684034+kunimisun@users.noreply.github.com>
2022-03-04 12:06:43 +0900

    Update script.js

--- script.js
+++ script.js
@@ -7,12 +7,38 @@ jQuery(function($) {
     });
     	
     $('.slider').slick({
-        autoplay:true,
-        autoplaySpeed:5000,
+        // autoplay:true,
+        // autoplaySpeed:5000,
         dots:true,
         slidesToShow:4,
-        slidesToScroll:1
-    });
+        slidesToScroll:1,
+        responsive:[
+                        {
+                            breakpoint: 1024,
+                            settings:{
+                                slidesToShow:3,
+                            }
+                        },
+                        {
+                            breakpoint: 768,
+                            settings:{
+                                slidesToShow:2,
+                            }
+                        },
+                        {
+                            breakpoint: 480,
+                            settings:{
+                                slidesToShow:1,
+                            }
+                        },
+                    ]
+                });
+        
+        
+        
+            });
+   
+    
 
 
     $(function(){
@@ -45,7 +71,7 @@ jQuery(function($) {
             });
         });
     });
-    });
+
     
     
     
